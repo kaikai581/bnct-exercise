9b1b62143eaaa3221a5c7c3fc0f061d82fda78b9
Alpha Bootis <AlphaBootis@Alphas-MacBook-Pro.local>
2020-03-25 21:37:17 -0500

    ”geometry_almost_done”

--- first_exercise_with_b1/src/B1DetectorConstruction.cc
+++ first_exercise_with_b1/src/B1DetectorConstruction.cc
@@ -40,6 +40,19 @@
 #include "G4PVPlacement.hh"
 #include "G4SystemOfUnits.hh"
 
+//#include "globals.hh"
+#include "G4SubtractionSolid.hh"
+#include "G4Tubs.hh"
+
+#include "G4RotationMatrix.hh"
+#include "G4ThreeVector.hh"
+
+#include <cmath>
+
+//#include "globals.hh"
+//#include "G4ThreeVector.hh"
+//#include <CLHEP/Vector/Rotation.h>
+
 // Eye candy headers
 #include "G4VisAttributes.hh"
 #include "G4Colour.hh"
@@ -124,16 +137,42 @@ G4VPhysicalVolume* B1DetectorConstruction::Construct()
   //  
   G4Material* shifter_mat = nist->FindOrBuildMaterial("G4_A-150_TISSUE");
   G4ThreeVector pos_shifter = G4ThreeVector(0, 0, (14/2)*cm);
+
+  //beam(10cm diameter, 82cm long) plus target(2cm lithium)
+   G4double BeaminnerRadius = 0.*cm; G4double BeamouterRadius = 10/2.*cm; G4double Beamhz = (82+2)/2.*cm;
+   G4double BeamstartAngle = 0.*deg; G4double BeamspanningAngle = 360.*deg;
+   G4Tubs* BeamPlusTar
+     = new G4Tubs("BeamPlusTar",
+               BeaminnerRadius,
+               BeamouterRadius,
+               Beamhz,
+               BeamstartAngle,
+               BeamspanningAngle);
+
+  //positon and orientation of BeamTarget relative to shiter cone 
+   G4RotationMatrix* yBeamRot = new G4RotationMatrix; // Rotates X and Z axes only 
+    yBeamRot->rotateY(-M_PI/4.*rad); // Rotates 45 degrees 
+  // yBeamRot->rotateY(0.*rad); // Rotates 45 degrees 
+   // G4ThreeVector zBeamTrans(0, 0, 0);   
+   //G4ThreeVector zBeamTrans(((((-84-2)/2.)+1.)/sqrt(2))*cm, 0, (-((14+82+2)/2.)+1.)*cm); 
+   
+   G4ThreeVector zxBeamTrans(-(41./(sqrt(2)))*cm, 0, (-((14+82+2)/2.)+1.)*cm);
         
   // Conical section for shifter       
-  G4double shifter_rmina =  0.*cm, shifter_rmaxb = (6+43/3.)*cm;
-  G4double shifter_rminb =  0.*cm, shifter_rmaxa = 25.*cm;
-  G4double shifter_hz = (14/2)*cm;
-  G4double shifter_phimin = 0.*deg, shifter_phimax = 360.*deg;
-  G4Cons* solidShifter =    
-    new G4Cons("Shifter", 
-    shifter_rmina, shifter_rmaxa, shifter_rminb, shifter_rmaxb, shifter_hz,
-    shifter_phimin, shifter_phimax);
+  G4double shifterwosubeam_rmina =  0.*cm, shifterwosubeam_rmaxb = (6+43/3.)*cm;
+  G4double shifterwosubeam_rminb =  0.*cm, shifterwosubeam_rmaxa = 25.*cm;
+  G4double shifterwosubeam_hz = (14/2)*cm;
+  G4double shifterwosubeam_phimin = 0.*deg, shifterwosubeam_phimax = 360.*deg;
+  G4Cons* solidShifterwosubeam =    
+    new G4Cons("Shifterwosubeam", 
+    shifterwosubeam_rmina, shifterwosubeam_rmaxa, shifterwosubeam_rminb, shifterwosubeam_rmaxb, 
+    shifterwosubeam_hz, shifterwosubeam_phimin, shifterwosubeam_phimax);
+
+  //Conical section for shifter subtract beamplustatget
+  G4SubtractionSolid* solidShifter =
+    new G4SubtractionSolid("solidShifter", solidShifterwosubeam, BeamPlusTar, yBeamRot, zxBeamTrans);
+
+
                       
   G4LogicalVolume* logicShifter =                         
     new G4LogicalVolume(solidShifter,         //its solid
@@ -149,6 +188,11 @@ G4VPhysicalVolume* B1DetectorConstruction::Construct()
                     false,                   //no boolean operation
                     0,                       //copy number
                     checkOverlaps);          //overlaps checking
+
+
+
+
+
 
   // //     
   // // Shape 2
@@ -263,7 +307,6 @@ G4VPhysicalVolume* B1DetectorConstruction::Construct()
                         "Thneuabs");           //its name
   logicThneuabs->SetVisAttributes(new G4VisAttributes(G4Colour::Red()));
 
-
                
   new G4PVPlacement(0,                       //no rotation
                     pos_thneuabs,                    //at position
@@ -273,6 +316,153 @@ G4VPhysicalVolume* B1DetectorConstruction::Construct()
                     false,                   //no boolean operation
                     0,                       //copy number
                     checkOverlaps);          //overlaps checking
+
+
+
+// full cone minus poly-Li(delimiter)
+  //  
+  //G4Material* aircone_mat = nist->FindOrBuildMaterial("G4_Cd"); //Cd
+  G4ThreeVector pos_fullcone_minus_polyLi = G4ThreeVector(0, 0, ((14+30+3.5+0.1+9.4-2.0)/2)*cm);
+        
+  // Conical section minus poly-Li(delimiter)       
+  G4double fullconemp_rmina =  0.*cm, fullconemp_rmaxa = (50/2.)*cm;
+  G4double fullconemp_rminb =  0.*cm, fullconemp_rmaxb = (6+2/3)*cm;
+  G4double fullconemp_hz = ((14+30+3.5+0.1+9.4-2.0)/2)*cm;
+  G4double fullconemp_phimin = 0.*deg, fullconemp_phimax = 360.*deg;
+  G4Cons* solidFullconemp =    
+    new G4Cons("Fullconemp", 
+    fullconemp_rmina, fullconemp_rmaxa, fullconemp_rminb, fullconemp_rmaxb, fullconemp_hz,
+    fullconemp_phimin, fullconemp_phimax);
+
+
+  // right side Pd(reflector) minus poly-Li(delimiter)
+  //  
+  G4Material* rside_reflector = nist->FindOrBuildMaterial("G4_Pb"); //Pb
+  G4ThreeVector rside_refle_minus_polyLi = G4ThreeVector(0, 0, ((14+30+3.5+0.1+9.4-2.0)/2)*cm);
+        
+  // right side box minus poly-Li(delimiter) length
+  G4double rsbmp_sizeXY = 70.*cm;
+  G4double rsbmp_sizeZ  = (14+30+3.5+0.1+9.4-2.0)*cm;      
+
+  G4Box* solidRsbmp =    
+   new G4Box("RsideEnvelopemp",                    //its name
+       0.5*rsbmp_sizeXY, 0.5*rsbmp_sizeXY, 0.5*rsbmp_sizeZ); //its size
+
+  G4SubtractionSolid* RsbsubtconeminusPL =
+   new G4SubtractionSolid("RsbsubtconeminusPL", solidRsbmp, solidFullconemp);
+
+
+  G4LogicalVolume* logicRsbsubtconeminusPL =                         
+    new G4LogicalVolume(RsbsubtconeminusPL,         //its solid
+                        rside_reflector,          //its material
+                        "RsbsubtconeminusPL");               //its name
+  logicRsbsubtconeminusPL->SetVisAttributes(new G4VisAttributes(G4Colour(204/255.,0/255.,204/255.)));
+              
+  new G4PVPlacement(0,                       //no rotation
+                    rside_refle_minus_polyLi,                    //at position
+                    logicRsbsubtconeminusPL,             //its logical volume
+                    "RsbsubtconeminusPL",                //its name
+                    logicWorld,                //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking
+
+
+
+// poly-Li(delimiter) (polyethylene)
+  //  
+  G4Material* delimiter_mat = nist->FindOrBuildMaterial("G4_Pb"); //Pb
+  G4ThreeVector pos_delimiter = G4ThreeVector(0, 0, (14+30+3.5+0.1+9.4-2.0+(2.0)/2)*cm);
+        
+  // poly-Li(delimiter) length
+  G4double delimiter_sizeXY = 70.*cm;
+  G4double delimiter_sizeZ  = 2.*cm;   
+
+  // last section cone 
+  //G4ThreeVector pos_fullcone_minus_polyLi = G4ThreeVector(0, 0, ((14+30+3.5+0.1+9.4-2.0)/2)*cm);
+      
+  G4double lasectioncone_rmina =  0.*cm, lasectioncone_rmaxb = (12/2.)*cm;
+  G4double lasectioncone_rminb =  0.*cm, lasectioncone_rmaxa = (6+2/3)*cm;
+  G4double lasectioncone_hz = (2/2.)*cm;
+  G4double lasectioncone_phimin = 0.*deg, lasectioncone_phimax = 360.*deg;
+  G4Cons* solidLasectioncone =    
+    new G4Cons("Lasectioncone", 
+    lasectioncone_rmina, lasectioncone_rmaxa, lasectioncone_rminb, lasectioncone_rmaxb, lasectioncone_hz,
+    lasectioncone_phimin, lasectioncone_phimax);  
+
+
+  G4Box* solidlastlid =    
+   new G4Box("lastlid",                    //its name
+       0.5*delimiter_sizeXY, 0.5*delimiter_sizeXY, 0.5*delimiter_sizeZ); //its size
+
+  G4SubtractionSolid* DelimiterLid =
+   new G4SubtractionSolid("DelimiterLid", solidlastlid, solidLasectioncone);
+
+
+  G4LogicalVolume* logicDelimiterLid =                         
+    new G4LogicalVolume(DelimiterLid,         //its solid
+                        delimiter_mat,          //its material
+                        "DelimiterLid");               //its name
+  logicDelimiterLid->SetVisAttributes(new G4VisAttributes(G4Colour(255/255.,0/255.,127/255.)));
+              
+  new G4PVPlacement(0,                       //no rotation
+                    pos_delimiter,                    //at position
+                    logicDelimiterLid,             //its logical volume
+                    "DelimiterLid",                //its name
+                    logicWorld,                //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking
+
+
+// left side reflector
+  
+  G4ThreeVector lside_refle_minus_beam = G4ThreeVector(0, 0, (-(14+30+3.5+0.1+9.4)/2)*cm);
+  //positon and orientation of BeamTarget relative to left box 
+    G4RotationMatrix* LsideyBeamRot = new G4RotationMatrix; // Rotates X and Z axes only 
+    LsideyBeamRot->rotateY(-(M_PI/4.)*rad); // Rotates 45 degrees 
+    // yBeamRot->rotateY(0.*rad); // Rotates 45 degrees 
+    // G4ThreeVector zBeamTrans(0, 0, 0);   
+    //G4ThreeVector zBeamTrans(((((-84-2)/2.)+1.)/sqrt(2))*cm, 0, (-((14+82+2)/2.)+1.)*cm); 
+  
+    G4ThreeVector LsidezxBeamTrans(-(41.0/(sqrt(2)))*cm, 0, 0.*cm);
+  //  G4ThreeVector LsidezxBeamTrans(-(12.5/(sqrt(2)))*cm, 0, (1.-((82+2-57)/2.))*cm);    
+
+
+  // left side box 
+  G4double leftsb_sizeXY = 70.*cm;
+  G4double leftsb_sizeZ  = (14+30+3.5+0.1+9.4)*cm;      
+
+  G4Box* solidLeftsb =    
+   new G4Box("Leftsb",                    //its name
+       0.5*leftsb_sizeXY, 0.5*leftsb_sizeXY, 0.5*leftsb_sizeZ); //its size
+
+  //Left side box subtract beamplustatget
+  G4SubtractionSolid* LeftsbminusBeam =
+    new G4SubtractionSolid("LeftsbminusBeam", solidLeftsb, BeamPlusTar, LsideyBeamRot, LsidezxBeamTrans);
+     
+
+
+
+
+  G4LogicalVolume* logicLeftsbminusBeam =                         
+    new G4LogicalVolume(LeftsbminusBeam,         //its solid
+                        rside_reflector,          //its material
+                        "LeftsbminusBeam");               //its name
+  logicLeftsbminusBeam->SetVisAttributes(new G4VisAttributes(G4Colour(204/255.,0/255.,204/255.)));
+              
+  new G4PVPlacement(0,                       //no rotation
+                    lside_refle_minus_beam,                    //at position
+                    logicLeftsbminusBeam,             //its logical volume
+                    "LeftsbminusBeam",                //its name
+                    logicWorld,                //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking
+
+
+
+
 
   // Set Shape2 as scoring volume
   //
