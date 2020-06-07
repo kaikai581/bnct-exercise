021e4570016608dbb611be67fb40bee8d06bccaf
Shih-Kai Lin <kaikai5816787@gmail.com>
2020-06-07 13:17:30 -0500

    A compilable CMake project from scratch.

--- NFromTarget/neutron_from_target.cc
+++ NFromTarget/neutron_from_target.cc
@@ -1,2 +1,10 @@
 /// \file neutron_from_target.cc
 /// \brief Main program of the simple study of neutron spectrum out of protons on target.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hi" << endl;
+}
